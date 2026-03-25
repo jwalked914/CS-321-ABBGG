e304c0b867fd3cf6a950bbc920c677fc0ba08685
nk0054 <nk0054@uah.edu>
2026-03-25 12:33:15 -0500

    User class

---
+++ src/User.java
@@ -0,0 +1,93 @@
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ *  Represents a user and its core attributes.
+ *  Each User object stores a username, password, isAdmin boolean, collection ArrayList
+ *  and a review ArrayList.
+ */
+public class User
+{
+    private final String username;
+    private final String password;
+    private final Boolean isAdmin;
+    private final ArrayList<collections> collections;
+    // private final ArrayList<review> reviews;
+
+    /**
+     * Constructs a user object from various parameters
+     *
+     * @param username the username
+     * @param password the password
+     * @param isAdmin if the user is an admin
+     * @param collections an array that contains the user's collections
+     * @param review and array that contains the user's reviews
+     */
+
+    User(String username, String password, Boolean isAdmin, ArrayList<collections> collections,
+         ArrayList<review> reviews)
+    {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+        this.collections = new ArrayList<>();
+        // this.reviews = new ArrayList<>();
+    }
+
+    /**
+     * sets the username of the user when they are logging in for the first time
+     */
+
+    public void setUsername(String username)
+    {
+        User.username = username;
+    }
+
+    /**
+     * returns the user's username
+     *
+     * @return the username
+     */
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    /**
+     * returns the user's password
+     *
+     * @return the password
+     */
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    /**
+     * sets the password of the user when they are logging in for the first time
+     */
+
+    public void setPassword()
+    {
+        User.password = password;
+    }
+
+    /**
+     * returns if the user is an admin
+     *
+     * @return isAdmin
+     */
+
+    public Boolean getIsAdmin()
+    {
+        return isAdmin;
+    }
+
+    /**
+     * returns
+     */
+
+}
