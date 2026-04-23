8ec4cebbb935192b801147d8370e407d5153d03d
numeo06 <bnheng06@gmail.com>
2026-04-23 10:48:27 -0500

    Document main method with JavaDoc comments
    Added JavaDoc comments to the main method for clarity.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,16 @@ import viewAndControl.LoginScreen;
 
 import javax.swing.*;
 
-void main()
+/**
+ * Entry point for the application.
+ * This method initializes the application by loading game data from an XML
+ * file into a GameDatabase loading user data from an XML file into a UserDatabase
+ * by providing the path for review data
+ * launching the LoginScreen
+
+ * @param args command-line arguments (it is not used)
+ */
+public static void main(String[] args)
 {
     SwingUtilities.invokeLater(() ->
     {
@@ -15,12 +24,12 @@ void main()
         File usersFile = new File("resources/usersInfo.xml");
         String reviewsPath = "resources/reviews.xml";
 
-        //Load in model.Game Database
+        //Load in Game Database
         FileScannerXML gameScanner = new FileScannerXML(gamesFile, null);
         ArrayList<Game> gameList = gameScanner.parseGamesFromXML();
         GameDatabase gameDatabase = new GameDatabase(gameList);
 
-        //Load in model.User Database
+        //Load in User Database
         FileScannerXML userScanner = new FileScannerXML(usersFile, gameDatabase);
         ArrayList<User> userList = userScanner.parseUsersFromXML();
         UserDatabase userDatabase = new UserDatabase(usersFile, gameDatabase);
